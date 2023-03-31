1294923407968d87e6bc293c04ae9345c8ce563a
zeppoo <reinverhaag@gmail.com>
2023-04-01 01:04:55 +0200

    01/04/2023

--- Super Brawlhalla stars/Assets/Players/Player 1/P1Attack.cs
+++ Super Brawlhalla stars/Assets/Players/Player 1/P1Attack.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class P1Attack : MonoBehaviour
 {
+    public Transform upAttack;
+    public Transform frontAttack;
+    public Transform downAttack;
     public Transform attackPoint;
     public float attackRange = 0.5f;
     public bool attackStat;
@@ -11,17 +14,31 @@ public class P1Attack : MonoBehaviour
     public float attackDamage;
     public float attackCooldown = 0.7f;
     public float attackCounter;
-    
+    public GameObject hand;
+
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        hand = GameObject.Find("P1 Hand");
     }
 
     // Update is called once per frame
     void Update()
     {
+        if (Input.GetKey(KeyCode.S))
+        {
+            attackPoint = downAttack;
+            
+
+        } else if (Input.GetKey(KeyCode.W))
+        {
+            attackPoint = upAttack;
+
+        } else {attackPoint = frontAttack;}
+
+        hand.transform.position = attackPoint.position; //draws hand at current attack position
+
         if (Input.GetKey(KeyCode.H) && attackStat == true)
         {
             attackStat = false;
@@ -47,6 +64,20 @@ public class P1Attack : MonoBehaviour
         foreach(Collider2D enemy in hitEnemies)
         {
             Debug.Log("Succesful hit");
+            if (attackPoint == upAttack)
+            {
+                enemy.GetComponent<P2Health>().knockbackPowerUp = 10;
+                enemy.GetComponent<P2Health>().knockbackPower = 1;
+            } else if (attackPoint == downAttack)
+            {
+                enemy.GetComponent<P2Health>().knockbackPowerUp = -2;
+                enemy.GetComponent<P2Health>().knockbackPower = 2;
+            } else if (attackPoint == frontAttack)
+            {
+                enemy.GetComponent<P2Health>().knockbackPowerUp = 5;
+                enemy.GetComponent<P2Health>().knockbackPower = 8;
+            }
+
 
             enemy.GetComponent<P2Health>().TakeDamage(attackDamage);
         }
